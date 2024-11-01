9aed35d6053967776b92e792cfda8aad43c8a4db
Mohd Shawez Khan <shawezkhan297@gmail.com>
2024-11-01 20:42:02 +0530

    Update crew.py

--- src/newsletter_gen/crew.py
+++ src/newsletter_gen/crew.py
@@ -1,6 +1,6 @@
 from crewai import Agent, Task, Crew, Process
 from crewai.project import CrewBase, agent, crew, task
-from newsletter_gen.tools.research import SearchAndContents, FindSimilar, GetContents 
+from research import SearchAndContents, FindSimilar, GetContents 
 from datetime import datetime
 import streamlit as st
 from typing import Union, List, Tuple, Dict
