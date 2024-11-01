0e3a7eb12d8df62d3fd17af1309ce34e0e8bd590
Mohd Shawez Khan <shawezkhan297@gmail.com>
2024-11-01 20:27:54 +0530

    Update main.py

--- src/newsletter_gen/main.py
+++ src/newsletter_gen/main.py
@@ -1,4 +1,4 @@
-from newsletter_gen.crew import NewsletterGenCrew
+from src.newsletter_gen.crew import NewsletterGenCrew
 
 def load_html_template():
     with open('Newsletter_crewAI\src\newsletter_gen\config\newsletter_template.html','r') as file:
